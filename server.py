f439b9c8217158fe3840a148c6d2b7c67208ea60
iWanderling <slyvkanikitao@gmail.com>
2024-04-28 23:12:49 +0500

    Commit All Changes

---
+++ server.py
@@ -0,0 +1,67 @@
+from tools import get_geo_coordinates, get_distance
+from flask import Flask, request, jsonify
+import logging
+
+
+# Инициализация приложения и логирования:
+app = Flask(__name__)
+logging.basicConfig(level=logging.INFO, filename='app.log',
+                    format='%(asctime)s %(levelname)s %(name)s %(message)s')
+
+
+# Функция для отправки данных на сервер:
+@app.route('/post', methods=['POST'])
+def main():
+    logging.info('Request: %r', request.json)
+    response = {
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+            'end_session': False
+        }
+    }
+    handle_dialog(response, request.json)
+    logging.info('Request: %r', response)
+    return jsonify(response)
+
+
+# Обработчик диалога с пользователем:
+def handle_dialog(res, req):
+    if req['session']['new']:
+        res['response']['text'] = ('Добро пожаловать! Я - помощник, который может показать вам город, '
+                                   'а также отправить вам расстояние между выбранными городами!')
+        return
+
+    # Получение городов:
+    cities = get_cities(req)
+
+    if not cities:
+        res['response']['text'] = 'Вы не написали название города(-ов)!'
+
+    elif len(cities) == 1:
+        res['response']['text'] = ('Данный город находится в следующей стране:' +
+                                   get_geo_coordinates(cities[0], 'country'))
+
+    elif len(cities) == 2:
+        distance = get_distance(get_geo_coordinates(cities[0], 'coordinates'),
+                                get_geo_coordinates(cities[1], 'coordinates'))
+        res['response']['text'] = 'Расстояние между данными городами: ' + \
+                                  str(round(distance)) + ' км.'
+    else:
+        res['response']['text'] = 'Вы отправили слишком много городов!'
+
+
+def get_cities(req):
+    cities = []
+
+    for entity in req['request']['nlu']['entities']:
+
+        if entity['type'] == 'YANDEX.GEO':
+            if 'city' in entity['value']:
+                cities.append(entity['value']['city'])
+
+    return cities
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ tools.py
@@ -0,0 +1,65 @@
+import requests, math
+
+
+def get_distance(p1, p2):
+    # p1 и p2 - это кортежи из двух элементов - координаты точек
+    radius = 6373.0
+
+    lon1 = math.radians(p1[0])
+    lat1 = math.radians(p1[1])
+    lon2 = math.radians(p2[0])
+    lat2 = math.radians(p2[1])
+
+    d_lon = lon2 - lon1
+    d_lat = lat2 - lat1
+
+    a = math.sin(d_lat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(d_lon / 2) ** 2
+    c = 2 * math.atan2(a ** 0.5, (1 - a) ** 0.5)
+
+    distance = radius * c
+    return distance
+
+
+def get_geo_coordinates(city_name, type_info):
+    # Получаем страну города:
+    if type_info == 'country':
+        try:
+            url = "https://geocode-maps.yandex.ru/1.x/"
+            params = {
+                "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+                'geocode': city_name,
+                'format': 'json'
+            }
+            data = requests.get(url, params).json()
+            # все отличие тут, мы получаем имя страны
+            return data['response']['GeoObjectCollection'][
+                'featureMember'][0]['GeoObject']['metaDataProperty'][
+                'GeocoderMetaData']['AddressDetails']['Country']['CountryName']
+        except Exception as e:
+            status_error = e
+            return
+
+    # Получаем координаты города:
+    elif type_info == 'coordinates':
+        try:
+            url = "https://geocode-maps.yandex.ru/1.x/"
+            params = {
+                "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+                'geocode': city_name,
+                'format': 'json'
+            }
+
+            response = requests.get(url, params)
+            json = response.json()
+
+            coordinates_of_city = json['response']['GeoObjectCollection']['featureMember'][0]
+            ['GeoObject']['Point']['pos'].split()
+
+            long, lat = float(coordinates_of_city[0]), float(coordinates_of_city[1])
+            return long, lat
+
+        except Exception as e:
+            status_error = e
+            return
+
+    return
